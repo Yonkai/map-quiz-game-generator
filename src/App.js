e936e258350c5377a8c0266330f715764de49776
Frank <fgunsch.webdev@gmail.com>
2020-01-01 02:57:18 -0500

    add hackiness

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import HongKongModified from "./custom-map-test/hong-kong-modified/index";
+import { CheckboxSVGMap } from "react-svg-map";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { regionsClicked:[] }
+      // This binding is necessary to make `this` work in the callback
+      this.handleClick = this.handleClick.bind(this);
+    }
+  
+    handleClick(e) {
+      const regionsClickedSet = e.map((path,index)=> {
+        return path.attributes[6].value;
+      })
+      this.setState(regionsClicked => ({
+        regionsClicked: regionsClickedSet
+      }));
+    }  
+
+  render() { 
+    return (
+    <div className='main-container'>
+        <div className='svg-map-grid-item'>
+      <h1 className>Hong Kong Geography Quiz Game</h1>
+        {/* {this.state.map()} */}
+        <CheckboxSVGMap map={HongKongModified} onChange={(e) => this.handleClick(e)}/>
+          <h2>{`Click on <randomly selected area>`}</h2>
+          <ul>
+          {this.state.regionsClicked.map((name,index)=>{
+            return <li>{name}</li>
+          })}
+          </ul>
+        </div>
+      </div>
+      );
+  }
 }
-
+ 
 export default App;
