9379a3477bf991c11086e1dbc19be006d7ce20e8
Frank <fgunsch.webdev@gmail.com>
2020-01-01 20:25:33 -0500

    add notes

--- src/App.js
+++ src/App.js
@@ -24,10 +24,24 @@ class App extends Component {
     return (
     <div className='main-container'>
         <div className='svg-map-grid-item'>
-      <h1 className>Hong Kong Geography Quiz Game</h1>
+      <h1 className>Geography Quiz Game</h1>
         {/* {this.state.map()} */}
         <CheckboxSVGMap map={PlainMapTest} onChange={(e) => this.handleClick(e)}/>
-          <h2>{`Click on <randomly selected area>`}</h2>
+          <p>{`% correct <total guesses/current guess position in names being looped through)>`}</p>
+          <p>{`timer after hiting start`}</p>
+          <p>{`Click on <randomly selected area>`}</p>
+        <button>{`Start button`}</button>
+          {/* How its played:
+              1. Each area intitially starts out same color representing not gussed yet. (green)
+              2. After a 1st CORRECT guess the area turns white.
+              3. After a 2nd-4th CORRECT guess the area turns blue.
+              4. After a 5th+ CORRECT guess the area turns red.
+              5. When 5 incorrect guesses are made in a row area flashes showing the correct area
+              6. A name appears over the area if it is incorrectly guessed, showings its name.
+              7. The name that needs to be guessed follows the cursor.
+              8. After every area has been given a guess a popover(modal?) appears and shows the score
+                 and ranking.
+          */}
           <ul>
           {this.state.regionsClicked.map((name,index)=>{
             return <li>{name}</li>
