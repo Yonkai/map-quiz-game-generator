0975de9d0b69d09daf5ebc252ae690aef12b3c29
Frank <fgunsch.webdev@gmail.com>
2020-01-12 21:30:40 -0500

    add deployment scripts for ghpages

--- src/App.test.js
+++ src/App.test.js
@@ -33,4 +33,4 @@ import App from './App';
 
 //TODO test: Make sure that the string interpolation util functions return expected output for given expected inputs
 
-//TODO play through thousands/millions of games to check for error programmitcally.
+//TODO play through thousands/millions of games to check for errors programmitcally.
