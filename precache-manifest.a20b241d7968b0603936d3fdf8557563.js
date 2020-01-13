39008488073e6400afebef095949d6ca47f099f7
Frank <fgunsch.webdev@gmail.com>
2020-01-12 23:37:58 -0500

    Updates

---
+++ precache-manifest.a20b241d7968b0603936d3fdf8557563.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b0920bb3f27aea81d3f6e38ab04742b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31134418e1af323f72b1",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "c48b86c5710d299ddc95",
+    "url": "/static/js/2.72956d82.chunk.js"
+  },
+  {
+    "revision": "7ae014860e02ae3f44f567e22891031b",
+    "url": "/static/js/2.72956d82.chunk.js.LICENSE"
+  },
+  {
+    "revision": "31134418e1af323f72b1",
+    "url": "/static/js/main.a6418152.chunk.js"
+  },
+  {
+    "revision": "7aca6dd8b7b049caa85b",
+    "url": "/static/js/runtime-main.c6c8c50d.js"
+  }
+]);
