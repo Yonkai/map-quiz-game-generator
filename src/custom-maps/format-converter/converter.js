b753fc427ed951f06b78247c199c75c519ad041d
Frank <fgunsch.webdev@gmail.com>
2020-03-08 23:50:27 -0400

    expand converter file

--- src/custom-maps/format-converter/converter.js
+++ src/custom-maps/format-converter/converter.js
@@ -1,1 +1,10 @@
 //TODO: Write converter for svg files
+import Canada from '../Canada/index'
+
+//1. Convert imported JSON map to JavaScript object
+
+//2. Change "name" keys with spaces to be hyphened instead
+
+//3. Change all "id" keys to exactly match "name" keys 
+
+//4. export the function 
