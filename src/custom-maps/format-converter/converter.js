c959226033895ae2b4a2413145a3f83e0971f35f
Frank <fgunsch.webdev@gmail.com>
2020-03-12 23:56:28 -0400

    add syntax

--- src/custom-maps/format-converter/converter.js
+++ src/custom-maps/format-converter/converter.js
@@ -1,11 +1,16 @@
 //TODO: Write converter for svg files
-import Canada from '../Canada/index'
+import Canada from '../nodeCanada/index.js'
 
-//1. Convert imported JSON map to JavaScript object
-let obj = JSON.parse(Canada);
+function converter(){    
+    //1. Convert imported JSON map to JavaScript object
+    let obj = JSON.parse(Canada);
+    console.log(obj)
+    
+    //3. Change all "id" keys to exactly match "name" keys 
+    
+    //4. export the function
+}
 
-//3. Change all "id" keys to exactly match "name" keys 
-
-//4. export the function
+converter();
 
 export default converter; 
